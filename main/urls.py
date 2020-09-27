8b7e4f58b79561242c4b7dd712c581c93220201f
toriumi <ken.tennis914@gmail.com>
2020-09-27 20:31:02 +0900

    add index

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
+import main
 from main import views
 
 app_name = 'api'
 
 urlpatterns = [
     path('terms-of-service/', views.terms_of_service, name='terms_of_service'),
-    path('', views.top, name='top'),
 ]
+
+# catch all other URL
+urlpatterns += [re_path(r'^.*/$', main.views.top, name='top')]
+urlpatterns += [path('', main.views.top, name='top')]
