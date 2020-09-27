5845e40140f785988723848ad2b0ca601aad5ab9
toriumi <ken.tennis914@gmail.com>
2020-09-27 20:42:39 +0900

    add index

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,6 @@ from main import views
 app_name = 'api'
 
 urlpatterns = [
-    path('', views.terms_of_service, name='terms_of_service'),
+    path('', views.top, name='terms_of_service'),
     path('terms-of-service/', views.terms_of_service, name='terms_of_service'),
 ]
