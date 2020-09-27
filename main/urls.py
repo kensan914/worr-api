67311d7dd72604f97b4e4e11229f1ba1f3039294
toriumi <ken.tennis914@gmail.com>
2020-09-27 20:43:43 +0900

    add index

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, re_path
 import main
 from main import views
 
-app_name = 'api'
+app_name = 'main'
 
 urlpatterns = [
-    path('', views.top, name='terms_of_service'),
+    path('', views.top, name='top'),
     path('terms-of-service/', views.terms_of_service, name='terms_of_service'),
 ]
