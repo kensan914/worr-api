56896dfc4b462b3d2faff9addb41988ed052b83a
toriumi <ken.tennis914@gmail.com>
2020-09-27 20:34:46 +0900

    add index

--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,3 @@ app_name = 'api'
 urlpatterns = [
     path('terms-of-service/', views.terms_of_service, name='terms_of_service'),
 ]
-
-# catch all other URL
-urlpatterns += [re_path(r'^.*/$', main.views.top, name='top')]
-urlpatterns += [path('', main.views.top, name='top')]
