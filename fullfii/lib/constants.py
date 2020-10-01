5a4392db1ef3bece25254cc2741a2b8401ae31b1
toriumi <ken.tennis914@gmail.com>
2020-10-01 20:28:38 +0900

    update 1.0.0

--- fullfii/lib/constants.py
+++ fullfii/lib/constants.py
@@ -1,4 +1,5 @@
 from config import settings
 
 ### URL ###
-BASE_URL = 'http://192.168.11.2:8080/' if settings.DEBUG else 'https://fullfii.com/'
+# BASE_URL = 'http://192.168.11.2:8080/' if settings.DEBUG else 'https://fullfii.com/'
+BASE_URL = 'https://fullfii.com/'
