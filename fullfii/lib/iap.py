7e3a9aaea10ada2f5e30a9ea0dc70a054d8771a5
toriumi <ken.tennis914@gmail.com>
2020-10-06 21:07:04 +0900

    update iap

--- fullfii/lib/iap.py
+++ fullfii/lib/iap.py
@@ -1,23 +1,24 @@
 from rest_framework import status
 from rest_framework.response import Response
-import fullfii
 from account.models import Plan, Iap
 import requests
 from account.serializers import MeSerializer
 from fullfii import create_iap
+from fullfii.lib.constants import IAP_SHARED_SECRET, IAP_STORE_API_URL, IAP_STORE_API_URL_SANDBOX, BUNDLE_ID
+from fullfii.lib.support import cvt_tz_str_to_datetime
 
 
 def request_post_receipt(receipt):
     post_data = {
         'receipt-data': receipt,
-        'password': fullfii.IAP_SHARED_SECRET,
+        'password': IAP_SHARED_SECRET,
         'exclude-old-transactions': True,
     }
-    res = requests.post(fullfii.IAP_STORE_API_URL, json=post_data)
+    res = requests.post(IAP_STORE_API_URL, json=post_data)
     res_json = res.json()
 
     if res_json['status'] == 21007:  # sandbox
-        res = requests.post(fullfii.IAP_STORE_API_URL_SANDBOX, json=post_data)
+        res = requests.post(IAP_STORE_API_URL_SANDBOX, json=post_data)
         res_json = res.json()
 
     return res_json
@@ -32,7 +33,7 @@ def verify_receipt_when_purchase(product_id, receipt, user):
 
     if res_json['status'] != 0:
         return Response({'type': 'failed_verify_receipt', 'message': "bad status"}, status=status.HTTP_409_CONFLICT)
-    if res_json['receipt']['bundle_id'] != fullfii.BUNDLE_ID:
+    if res_json['receipt']['bundle_id'] != BUNDLE_ID:
         return Response({'type': 'failed_verify_receipt', 'message': "bad bundle ID"}, status=status.HTTP_409_CONFLICT)
     if Iap.objects.filter(
             original_transaction_id=res_json['latest_receipt_info'][0]['original_transaction_id']).exists():
@@ -47,7 +48,7 @@ def verify_receipt_when_purchase(product_id, receipt, user):
         transaction_id=res_json['latest_receipt_info'][0]['transaction_id'],
         user=user,
         receipt=res_json['latest_receipt'],
-        expires_date=fullfii.cvt_tz_str_to_datetime(res_json['latest_receipt_info'][0]['expires_date']),
+        expires_date=cvt_tz_str_to_datetime(res_json['latest_receipt_info'][0]['expires_date']),
     )
     user.plan = product_id
     user.save()
