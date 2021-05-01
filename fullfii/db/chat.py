e6ca51a3bd25901529f538bcda0e8e69b8e7a2c2
toriumi <ken.tennis914@gmail.com>
2021-05-01 22:05:25 +0900

    悩みジャンルの絞り込み

--- fullfii/db/chat.py
+++ fullfii/db/chat.py
@@ -1,3 +1,4 @@
+from account.models import GenreOfWorries
 import random
 from chat.models import TalkStatus, TalkTicket
 
@@ -29,3 +30,32 @@ def activate_talk_ticket(talk_ticket):
     talk_ticket.status = TalkStatus.STOPPING
     talk_ticket.save()
     return talk_ticket
+
+
+def gene_length_participants():
+    length_participants = {}
+    for genreOfWorry in GenreOfWorries.objects.all():
+        length_participants_by_worry = 0
+
+        participant_talkTicket = TalkTicket.objects.filter(
+            is_active=True,
+            owner__is_active=True
+        ).exclude(status=TalkStatus.STOPPING)
+        try:
+            # 悩みの絞り込み
+            if genreOfWorry.value == 'just_want_to_talk':
+                participant_talkTicket = participant_talkTicket.filter(
+                    worry=genreOfWorry)
+            else:
+                just_want_to_talk = GenreOfWorries.objects.get(
+                    value='just_want_to_talk')
+                participant_talkTicket = participant_talkTicket.exclude(
+                    worry=just_want_to_talk)
+        except:
+            participant_talkTicket = participant_talkTicket.filter(
+                worry=genreOfWorry)
+
+        length_participants_by_worry += participant_talkTicket.count()
+        length_participants[genreOfWorry.key] = length_participants_by_worry
+
+    return length_participants
